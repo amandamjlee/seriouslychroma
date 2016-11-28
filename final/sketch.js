2d718022e4516ebdc331f5fd3b812103f7ed0c0d
amandamjlee <amandaminjin.lee@gmail.com>
2016-11-28 15:09:57 -0500

    Add files via upload

--- final/sketch.js
+++ final/sketch.js
@@ -1,7 +1,7 @@
 
 var video;
 var img;
-
+var num;
 
 var indexfingerPos, indexfingerX, indexfingerY, indexfingerZ, lastindexfingerX, lastindexfingerY,lastindexfingerZ;
 var leftpalmPos, leftpalmPosX,leftpalmPosY,lastleftpalmPosX,lastleftpalmPosY;
@@ -20,11 +20,11 @@ function setup(){
 
 
 	// createCanvas(innerWidth, innerHeight);
-	background(0);
+	background(255);
 
 	// ellipse(mouseX,mouseY,20,20);
 
-	img= loadImage('circle.png');
+	img= loadImage('star.png');
 
 
 	var canvas = createCanvas(innerWidth, innerHeight);
@@ -33,10 +33,10 @@ function setup(){
 	
 	//seriously
 	canvas.id('p5canvas');
-	// canvas.style('size',)
+	
 
   	video = createCapture(VIDEO);
-  	video.size(2000,1000);
+  	video.size(2000,800);
     video.id('p5video');
    	video.hide();
     
@@ -51,9 +51,9 @@ function setup(){
     
     chroma.source= src;
     target.source= chroma;
-    var r = 0/255;
-    var g = 255/255;
-    var b = 0/255;
+    var r = 59/255;
+    var g = 190/255;
+    var b = 120/255;
     
     chroma.screen = [r,g,b,1];
     
@@ -73,27 +73,46 @@ function setup(){
 
 function draw(){
 	
-  	// fill(255,0,0);
-  	// ellipse(mouseX,mouseY, 20,20);
-	
+
 	//color boxs
 
 	//draw sparkle
 
-	// if(grab > 0.7){
-	// 	sparkle();
-	// }else{
-	// 	stop();
-	// }
+	if(grab > 0.7){
+		sparkle();
+	}else{
+		stop();
+	}
 	
 
 	//draw brush
 
 	if(handType=="right"&&handType!="left"){
 	 
-	 brush();	
 
-	}
+	   if (mouseIsPressed){
+
+	   	//rect brush
+	   brush(1);
+	  	
+	  	}else{
+	  	
+	  	//ellipse brush
+	    brush(3);
+		
+		}
+
+		// else if() {
+	 //  	//star brush
+	 //    brush(3);
+		
+		// }
+
+
+
+		}
+
+		
 
 	//draw erasor
 
@@ -103,17 +122,17 @@ function draw(){
 	}
 
 
-	tipPos();
+	// tipPos();
 
 
 
-	noStroke();
-	fill(255,0,0);
-	rect(10,10,100,100);
-	fill(255,200,0);
-	rect(10,150,100,100);
-	fill(0,200,255);
-	rect(10,290,100,100);
+	// noStroke();
+	// fill(255,0,0);
+	// rect(10,10,100,100);
+	// fill(255,200,0);
+	// rect(10,150,100,100);
+	// fill(0,200,255);
+	// rect(10,290,100,100);
 
 }
 
@@ -159,18 +178,22 @@ function sparkle(){
  	var palmMapX = map(leftpalmPosX, -220, 345, 0, width);
 	var palmMapY = map(leftpalmPosY, 0, 600, height, 0);
 
-	fill(random(200,255),random(200,255),random(150,250));
+	fill(random(200,255),random(170,200),random(80,150),random(80,250));
 	noStroke();
 	ellipse(random(palmMapX-300,palmMapX+300), random(palmMapY-300,palmMapY+300), random(0,10));
-
+	fill(random(200,255),random(50,140),random(100,200),random(80,250));
+	rect(random(palmMapX-310,palmMapX+300), random(palmMapY-300,palmMapY+300), random(0,10), random(0,10));
 
 }	
 
 
-function brush(){
+function brush(num){
 	
 
-	if(indexfingerPos != undefined){
+	
+		
+
+		if(indexfingerPos != undefined){
 
 	 	indexfingerX = indexfingerPos[0];
 	 	indexfingerY = indexfingerPos[1];
@@ -186,75 +209,86 @@ function brush(){
 		var mapLY = map(lastindexfingerY, 0, 600, height, 0);
 		var mapLZ = map(lastindexfingerZ, -250, 200, -130, 150);
 
-		var strokeSize = map(indexfingerZ , -250, 140, 80, 10);
+		var strokeSize = map(indexfingerZ , -250, 140, 50, 7);
 
 		lastindexfingerX = indexfingerX;
 	 	lastindexfingerY = indexfingerY;
 	 	lastindexfingerZ = indexfingerZ;
-
-		
 	
 
-		
-		stroke(255);
-		strokeWeight(strokeSize);
+		// image(img,mapCX,mapCY);
+		// stroke(255);
+		// strokeWeight(strokeSize);
 		// line(mapCX,mapCY,mapLX,mapLY);
 		
 	
 
 	console.log(indexfingerZ);
 
-//change color
-	
 
-	if(mapCX>10&&mapCX<110&&mapCY>10&&mapCY<110){
-		strokeR=true;
-		strokeY=false;
-		strokeB=false;
+	// if(strokeB==true){
+	// 	stroke(0,200,255);
 
-	}
-
-	if(strokeR==true){
-		stroke(255,0,0);
-
-	}
-
-	if(mapCX>10&&mapCX<110&&mapCY>150&&mapCY<250){
-		strokeY=true;
-		strokeR=false;
-		strokeB=false;
-	}
-
-	if(strokeY==true){
-		stroke(255,200,0);
-
-	}
-
-	if(mapCX>10&&mapCX<110&&mapCY>290&&mapCY<390){
-		strokeB=true;
-		strokeR=false;
-		strokeY=false;
-	}
-
-	if(strokeB==true){
-		stroke(0,200,255);
-
-	}
+	// }
 
 	if(indexfingerZ>95){
-		stroke(255,255,255,0);
+		stroke(255,0,0);
 		
 	}
 	
 //draw line
-
-	line(mapCX,mapCY,mapLX,mapLY);
 	
+		// noFill();
+
+	 	// strokeWeight(2);
+	
+	
+
+		if (num === 1 ){
+			
+			noStroke();
+			 	
+			if(indexfingerZ>95){
+				fill(255,0);
+				
+			}else{
+				fill(random(180,250),random(200,240),random(200,250));
+			}
+			
+			ellipse(mapCX,mapCY,mapCZ,mapCZ);
+
+		}else if(num===2){
+
+			noFill();
+			strokeWeight(2);
+				 	
+			if(indexfingerZ>95){
+				stroke(255,0);
+					
+			}else{
+				stroke(random(40,150),random(40,100),random(50,200));
+			}
+			
+			rect(mapCX,mapCY,mapCZ,mapCZ);
+
+
+		}else if(num===3){
+
+			
+			if(indexfingerZ>95){
+				stroke(255,0);
+				
+			}else {
+				stroke(255);
+			}
+			
+			strokeWeight(strokeSize);
+			
+			line(mapCX,mapCY,mapLX,mapLY);
+		}
 	}
 	
 }
-
-
 
 
 function tipPos(){
@@ -279,8 +313,8 @@ Leap.loop(function(frame) {
  		// handString = concatData("hand_type",hand.type)
 
 
-		console.log(handType);
-		console.log(frame);
+		// console.log(handType);
+		// console.log(frame);
 
  		// console.log(indexfingerPos);
 
